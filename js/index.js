90a593d2a629986cdc086f0ac460957967164110
kevit-yagnik-vora <yagnik.vora@kevit.io>
2025-07-16 13:27:27 +0530

    registration and login API done

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    hamburger.classList.toggle('open');
+});
+
+const token = localStorage.getItem('token');
+const user = JSON.parse(localStorage.getItem('userData'));
+
+if (!token || !user) {
+    window.location.href = './login.html';
+} else {
+    document.getElementById('username').textContent = user.name;
+}
+
+document.getElementById('logout-btn').addEventListener('click', () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    window.location.href = './login.html';
+});
