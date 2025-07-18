50a9eb8d8ee56564df463dbb7830c910f1d65c1a
kevit-yagnik-vora <yagnik.vora@kevit.io>
2025-07-18 12:25:37 +0530

    logo changed and style change to responsive and alert msg added

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
 
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-  hamburger.classList.toggle('open');
-});
 
 const token = localStorage.getItem('token');
 const user = JSON.parse(localStorage.getItem('userData'));
@@ -14,16 +8,6 @@ if (!token || !user) {
 } else {
   document.getElementById('username').textContent = user.name;
 }
-
-document.getElementById('logout-btn').addEventListener('click', () => {
-  localStorage.removeItem('token');
-  localStorage.removeItem('userData');
-  localStorage.setItem("dialogMessage", JSON.stringify({
-    text: "Logout successful!",
-    type: "info"
-  }));
-  window.location.href = './login.html';
-});
 
 window.addEventListener("DOMContentLoaded", () => {
   const msg = localStorage.getItem("dialogMessage");
